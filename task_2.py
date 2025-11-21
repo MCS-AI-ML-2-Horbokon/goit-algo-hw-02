266281cf14765077ca571279e62e18cd367f8efb
Andrii Horbokon <andrii.horbokon@gmail.com>
2025-11-21 22:29:28 +0200

    Task 2: ignore spaces and case

--- task_2.py
+++ task_2.py
@@ -5,7 +5,7 @@ def is_palindrome(word: str) -> bool:
     if len(word) < 2:
         return True
 
-    container = deque(word)
+    container = deque(word.replace(" ", "").lower())
     left = container.popleft()
     right = container.pop()
 
@@ -24,10 +24,10 @@ def is_palindrome(word: str) -> bool:
 if __name__ == "__main__":
     assert is_palindrome("")
     assert is_palindrome("a")
-    assert is_palindrome("aa")
+    assert is_palindrome("Aa")
     assert is_palindrome("aba")
-    assert is_palindrome("abba")
-    assert is_palindrome("abcba")
+    assert is_palindrome("abb  a")
+    assert is_palindrome("abcba ")
     assert not is_palindrome("ab")
     assert not is_palindrome("abb")
     assert not is_palindrome("abca")
